89104a207aa8ff101a51b691781bc7162d4f236a
Andrimarciely <andrimarciely@semed.manaus.am.gov.br>
2022-06-22 16:16:03 -0400

    fix -  index aluno coluna ações

--- views/aluno/index.php
+++ views/aluno/index.php
@@ -45,9 +45,21 @@ $this->params['breadcrumbs'][] = $this->title;
                                 return Html::img(Yii::getAlias('@web').'/img/'.$data['ALUNO_FOTO'],['width' =>'100px']);
                 },
             ],
-            //'TIPO_ALUNO_COD_FK',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
+            ['class' => 'yii\grid\ActionColumn',
+                        'template' => '{view} {update} {delete} {matricula} ',
+                        'header'=>'Ações',
+                        'headerOptions'=>[
+                            'style'=>'width:90px; text-align: center;'
+                        ],
+                        'buttons' => [
+                        'matricula' => function ($url, $model, $key) {
+                                        return Html::a( '<span class="glyphicon glyphicon-plus" ></span>',
+                                                        'index.php?r=matricula/create&id='.$model->ALUNO_COD_PK,
+                                                        ['title' => 'Matrícula']);
+                                        },
+                                    ],
+            ],
+          ],
+       ]);
+    ?>
 </div>
