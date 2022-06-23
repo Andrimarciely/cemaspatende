9cc23a1c577cfa6d701682f12045b6d889f64646
Andrimarciely <andrimarciely@semed.manaus.am.gov.br>
2022-06-23 14:31:17 -0400

    new feat matricula form

--- views/matricula/_form.php
+++ views/matricula/_form.php
@@ -11,15 +11,32 @@ use yii\widgets\ActiveForm;
 <div class="matricula-form">
 
     <?php $form = ActiveForm::begin(); ?>
+    <?php
 
-    <?= $form->field($model, 'MATRICULA_COD_PK')->textInput(['maxlength' => true]) ?>
+        if ($model->isNewRecord){
+            $idAluno = $_GET['idAluno'];
+            $modelAluno = ALUNO::find()->where(['ALUNO_COD_PK'=>$idAluno])->one();
+            $modelTipo = TIPOALUNO::find()->where(['TIPO_ALUNO_COD_PK'=>$modelAluno->TIPO_ALUNO_COD_FK])->one();
+            echo '<b>Código:</b>'.$idAluno.'</br>';
+            echo '<b>Nome:</b>'.$modelAluno->ALUNO_NOME.'</br>';
+            echo '<b>Tipo:</b>'.$modelTipo->TIPO_ALUNO_NOME.'</br>';
+            echo '<b>Tipo:</b>'.$modelTipo->TIPO_ALUNO_NOME.'</br>';
+            echo $form->field($model, 'ALUNO_COD_FK')->hiddenInput(['value'=>$idAluno])->label(false);
 
-    <?= $form->field($model, 'ALUNO_COD_FK')->textInput(['maxlength' => true]) ?>
+        }else{
+        
+            $id = $_GET['id'];
+            echo '<b>Código: </b>'.$id.'</br>';
+            echo '<b>Nome: </b>'.$model->aLUNOCODFK->ALUNO_NOME.'</br>';
+            echo '<b>Tipo: </b>'.$model->aLUNOCODFK->tIPOALUNOCODFK->TIPO_ALUNO_NOME.'</br>';
+            echo $form->field($model, 'ALUNO_COD_FK')->hiddenInput(['value'=>$model->aLUNOCODFK->ALUNO_COD_PK])->label(false);
+        }
+    ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
-
+   
     <?php ActiveForm::end(); ?>
 
 </div>
