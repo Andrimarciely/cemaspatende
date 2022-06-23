c93953d15fcf6440e27e974ca5446b98db467c39
Andrimarciely <andrimarciely@semed.manaus.am.gov.br>
2022-06-23 10:49:29 -0400

    new feat - views turma

--- views/turma/_form.php
+++ views/turma/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use app\models\TURMA;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\TURMA */
@@ -16,9 +18,39 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'TURMA_DT_INICIO')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'TURMA_DT_FIM')->textInput(['maxlength' => true]) ?>
+    <?php echo $form->field($model, 'TURMA_DT_INICIO')->widget(DatePicker::className(),[
+                        'name'=>'TURMA_DT_INICIO',
+                       // 'type' => DatePicker::TYPE_INPUT,
+                        'pluginOptions' => [
+                            'format' => 'dd/mm/yyyy',
+                            'autoclose'=>true,
+                            'todayHighlight' => true
+                        ],
+                    ]); 
+        ?>
 
-    <?= $form->field($model, 'CURSO_COD_FK')->textInput(['maxlength' => true]) ?>
+    <?php echo $form->field($model, 'TURMA_DT_FIM')->widget(DatePicker::className(),[
+                        'name'=>'TURMA_DT_FIM',
+                       // 'type' => DatePicker::TYPE_INPUT,
+                        'pluginOptions' => [
+                            'format' => 'dd/mm/yyyy',
+                            'autoclose'=>true,
+                            'todayHighlight' => true
+                        ],
+                    ]); 
+        ?>
+
+    
+    <?= $form->field($model, 'CURSO_COD_FK')->widget(Select2::classname(), [
+                        'data' => ArrayHelper::map(TIPOALUNO::find()->asArray()->all(), 'CURSO_COD_PK', 'CURSO_NOME'),
+
+                        'pluginOptions' => [
+                            'placeholder' => '- Selecione - ',
+                            'allowClear' => true,
+                        ]
+                    ]);
+                    ?>
+    
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
