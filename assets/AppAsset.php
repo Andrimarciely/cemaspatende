6f9da4ba1a72ea4b0fbc160a09497b4ef21b8c4d
Andrimarciely <andrimarciely@semed.manaus.am.gov.br>
2022-07-04 14:33:06 -0400

    correção do nome do arquivo

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -22,9 +22,6 @@ class AppAsset extends AssetBundle
     public $css = [
         'css/site.css',
     ];
-    public $js = [
-        'js/custom.js'
-    ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
