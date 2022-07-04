a83fab22ea2b860e4340a3793f30936a471f8723
Andrimarciely <andrimarciely@semed.manaus.am.gov.br>
2022-07-04 16:58:26 -0400

    correção de tags php

--- views/matricula/_form.php
+++ views/matricula/_form.php
@@ -16,25 +16,33 @@ use yii\helpers\ArrayHelper;
 <div class="matricula-form">
 
     <?php $form = ActiveForm::begin(); ?>
-    <?php
 
+    <!--<?= $form->field($model, 'MATRICULA_COD_PK')->textInput(['maxlength' => true]) ?>-->
+
+    <?php
         if ($model->isNewRecord){
             $idAluno = $_GET['idAluno'];
             $modelAluno = ALUNO::find()->where(['ALUNO_COD_PK'=>$idAluno])->one();
             $modelTipo = TIPOALUNO::find()->where(['TIPO_ALUNO_COD_PK'=>$modelAluno->TIPO_ALUNO_COD_FK])->one();
+
             echo '<b>Código:</b>'.$idAluno.'</br>';
             echo '<b>Nome:</b>'.$modelAluno->ALUNO_NOME.'</br>';
-            echo '<b>Tipo:</b>'.$modelTipo->TIPO_ALUNO_NOME.'</br>';
-            echo '<b>Tipo:</b>'.$modelTipo->TIPO_ALUNO_NOME.'</br>';
-            echo $form->field($model, 'ALUNO_COD_FK')->hiddenInput(['value'=>$idAluno])->label(false);
+            echo '<b>Tipo:</b>'.$modelAluno->TIPO_ALUNO_NOME.'</br>';
+    ?>
 
-        }else{
-        
+    <?=$form->field($model, 'ALUNO_COD_FK')->hiddenInput(['value'=>$idAluno])->label(false);?>
+    <?php
+        }
+        else{
             $id = $_GET['id'];
+        
             echo '<b>Código: </b>'.$id.'</br>';
             echo '<b>Nome: </b>'.$model->aLUNOCODFK->ALUNO_NOME.'</br>';
             echo '<b>Tipo: </b>'.$model->aLUNOCODFK->tIPOALUNOCODFK->TIPO_ALUNO_NOME.'</br>';
-            echo $form->field($model, 'ALUNO_COD_FK')->hiddenInput(['value'=>$model->aLUNOCODFK->ALUNO_COD_PK])->label(false);
+   
+    ?>       
+    <?=$form->field($model, 'ALUNO_COD_FK')->hiddenInput(['value'=>$model->aLUNOCODFK->ALUNO_COD_PK])->label(false);?>
+    <?php         
         }
     ?>
 
@@ -61,4 +69,4 @@ use yii\helpers\ArrayHelper;
    
     <?php ActiveForm::end(); ?>
 
-</div>
+    </div>
