b1b31e6a8363be53898e9263dfa3b174ae93c683
Andrimarciely <andrimarciely@semed.manaus.am.gov.br>
2022-07-06 15:50:10 -0400

    ajuste de matricula partindo do grid de aluno

--- views/matricula/_form.php
+++ views/matricula/_form.php
@@ -17,8 +17,41 @@ use kartik\select2\Select2;
 <div class="matricula-form">
 
     <?php $form = ActiveForm::begin(); ?>
+    <?php
+            if ($model->isNewRecord){
+                $idAluno = $_GET['id'];
+                $modelAluno = ALUNO::find()->where(['ALUNO_COD_PK'=>$idAluno])->one();
+                $modelTipo = TIPOALUNO::find()->where([
+                            'TIPO_ALUNO_COD_PK'=>$modelAluno->TIPO_ALUNO_COD_FK])->one();
+            echo '<b>Código:</b>'.$idAluno.'</br>';
+            echo '<b>Nome:</b>'.$modelAluno->ALUNO_NOME.'</br>';
+            echo '<b>Tipo:</b>'.$modelTipo->TIPO_ALUNO_NOME.'</br>'
+    ?>
+    <?= $form->field($model, 'ALUNO_COD_FK')->hiddenInput([
+            'value'=>$idAluno])->label(false);
+    ?>
+    <?php
+    }else{
+    $id = $_GET['id'];
+    echo '<b>Código: </b>'.$id.'</br>';
+    echo '<b>Nome: </b>'.$model->aLUNOCODFK->ALUNO_NOME.'</br>';
+    echo '<b>Tipo: </b>'.$model->aLUNOCODFK->tIPOALUNOCODFK->TIPO_ALUNO_NOME.'</br>';
+    ?>
+    <?= $form->field($model, 'ALUNO_COD_FK')->hiddenInput([
+        'value'=>$model->aLUNOCODFK->ALUNO_COD_PK])->label(false);
+    ?>
+    <?php
+}
+?>
 
-        <!--<?= $form->field($model, 'MATRICULA_COD_PK')->textInput(['maxlength' => true]) ?>-->
+<!--         <?= $form->field($model, 'ALUNO_COD_FK')->dropDownList(ArrayHelper::map(ALUNO::find()->all(), 
+                                                                                    'ALUNO_COD_PK', 
+                                                                                    'ALUNO_NOME'),
+                                                                                    [
+                                                                                        'prompt'=>'- Selecione - ',
+                                                                                        'disabled'=>!($model->isNewRecord),
+
+                                                                                    ]) ?>  -->
 
 
         <?= $form->field($model, 'CURSO_COD_FK')->dropDownList(ArrayHelper::map(CURSO::find()->all(), 
@@ -40,14 +73,6 @@ use kartik\select2\Select2;
                                                                                         'prompt'=>'- Selecione - ',
                                                                                         'disabled'=>!($model->isNewRecord),
                                                                                     ]) ?>
-        <?= $form->field($model, 'ALUNO_COD_FK')->dropDownList(ArrayHelper::map(ALUNO::find()->all(), 
-                                                                                    'ALUNO_COD_PK', 
-                                                                                    'ALUNO_NOME'),
-                                                                                    [
-                                                                                        'prompt'=>'- Selecione - ',
-                                                                                        'disabled'=>!($model->isNewRecord),
-
-                                                                                    ]) ?> 
 
 
         </div>                                                                                
