26f9fe71f6ddf0c8216b0ccf64a0d9da7eae76e0
Andrimarciely <andrimarciely@semed.manaus.am.gov.br>
2022-09-20 09:42:04 -0400

    modificação menu inicial

--- views/site/index.php
+++ views/site/index.php
@@ -31,9 +31,14 @@ $this->title = 'CEMASP ATENDE';
                 <div class="col-sm">
                     <center>
                         <a href="<?php echo Yii::$app->request->baseUrl; ?>/index.php?r=aluno/create">
-                            <div class="btn btn-default" style="width: 300px;" ><i class=" 	glyphicon glyphicon-import"></i> Inserção da Base de Aluno &raquo;</div>
+                            <div class="btn btn-default" style="width: 300px;" ><i class=" 	glyphicon glyphicon-import"></i> Inserção dos Dados SIGEAM &raquo;</div>
                         </a>
+                                              
+                        </br></br>
 
+                        <a href="<?php echo Yii::$app->request->baseUrl; ?>/index.php?r=aluno/index">
+                            <div class="btn btn-default" style="width: 300px;" ><i class="glyphicon glyphicon-folder-open"></i> Gerenciamento dos Dados CEMASP &raquo;</div>
+                        </a>
                         </br></br>
 
                         <a href="<?php echo Yii::$app->request->baseUrl; ?>/index.php?r=aluno/index">
@@ -70,5 +75,15 @@ $this->title = 'CEMASP ATENDE';
         <?php
         }
     }?>
+    </div>
+<div class="row"> </div>
+    <div class="footer-bg" style="background: url('https://www.manaus.am.gov.br/wp-content/themes/ppm/dist/imagens/2021/bg-rodape.png') repeat-x bottom left; "></div>
+	    <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+    		<div style="text-align: center; font-family: Calibri; color: #6c757d; font-size: 0.9em;">
+			    <img src="https://www.manaus.am.gov.br/wp-content/themes/ppm/dist/imagens/2021/logo-pmm-rodape.png"><br>
+			    <p>COPYRIGHT 2021. TODOS OS DIREITOS RESERVADOS</p>
+    	    </div>
+		</div>
+	    </div>
     </div>
 </div>
