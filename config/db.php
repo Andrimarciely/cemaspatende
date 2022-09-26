480a1e9ecd3a32d95a27624b90d79c16b337012b
Andrimarciely <andrimarciely@semed.manaus.am.gov.br>
2022-09-26 14:27:36 -0400

    atualização de referencia de bootstrap

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'oci:dbname=//localhost:1521/xe;charset=utf8',
-    'username' => 'as_scursos',
+    'username' => 'cemaspatende_semed',
     'password' => '123S.E.N.H.A.321',
     'charset' => 'UTF8',
     'tablePrefix' => 'TB_',
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -22,6 +22,16 @@ AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
+    
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> 
+    
     <?php $this->head() ?>
 </head>
 <body>
